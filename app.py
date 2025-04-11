f9d54302aa14b286520595e92c57853062d3adff
tarheelfan200 <tarheelfan3951@gmail.com>
2025-04-11 09:35:19 -0400

    push increment & decrement buttons - stable

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, jsonify
 from betterDataExtract import getAllLinesFromPDF, extractTableContent
 import sqlite3
 
@@ -21,6 +21,7 @@ def home():
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS shirts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            brand TEXT NOT NULL,
             description TEXT NOT NULL,
             color TEXT NOT NULL,
             size TEXT NOT NULL,
@@ -30,14 +31,43 @@ def home():
 
     # Pull data from database - set as shirts variable
     shirts = cursor.execute('''
-        SELECT brand, description, color, size, quantity FROM shirts;
+        SELECT id, brand, description, color, size, quantity FROM shirts;
     ''').fetchall()
 
     conn.close()
     return render_template("index.html", shirts=shirts)
 
+@app.route('/update_quantity', methods=['POST'])
+def update_quantity():
+    shirt_id = request.form['id']
+    action = request.form['action']
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    
+    # Get current quantity
+    cursor.execute('SELECT quantity FROM shirts WHERE id = ?', (shirt_id,))
+    current_quantity = cursor.fetchone()['quantity']
+    
+    # Update based on action
+    if action == 'increment':
+        new_quantity = current_quantity + 1
+    elif action == 'decrement':
+        new_quantity = max(0, current_quantity - 1)  # Prevent negative quantities
+    
+    # Update database
+    cursor.execute('''
+        UPDATE shirts
+        SET quantity = ?
+        WHERE id = ?
+    ''', (new_quantity, shirt_id))
+    
+    conn.commit()
+    conn.close()
+    
+    return redirect("/")
 
-# How to upload a file
+
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
